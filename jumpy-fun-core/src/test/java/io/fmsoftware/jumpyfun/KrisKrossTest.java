61f11ca2559e0db56d67e91b103fe7c6c1647913
Carl Fletcher <cfletch@gmail.com>
2020-02-28 16:17:21 -0500

    Added several unit tests

--- jumpy-fun-core/src/test/java/io/fmsoftware/jumpyfun/KrisKrossTest.java
+++ jumpy-fun-core/src/test/java/io/fmsoftware/jumpyfun/KrisKrossTest.java
@@ -1,21 +1,164 @@
 package io.fmsoftware.jumpyfun;
 
+import io.fmsoftware.jumpyfun.KrisKross;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 public class KrisKrossTest
 {
+    private static KrisKross jumpyLib = new KrisKross();
+    String json1 = "{ \"action\": \"run\", \"time\": 75 }";
+    String json2 = "{ \"action\": \"swim\", \"time\": 732 }";
+    String json3 = "{ \"action\": \"jump\", \"time\": 121 }";
+    String json4 = "{ \"action\": \"run\", \"time\": 200 }";
+    String json5 = "{ \"action\": \"ski\", \"time\": 543 }";
+    String json6 = "{ \"action\": \"jump\", \"time\": 200 }";
+    String json7 = "{ \"action\": \"run\", \"time\": 888 }";
+    String json8 = "{ \"action\": \"jump\", \"time\": 389 }";
+    String json9 = "{ \"action\": \"jump\", \"time\": 12 }";
+    String json10 = "{ \"action\": \"run\", \"time\": 75 }";
+
+    // negative values
+    String json11 = "{ \"action\": \"\", \"time\": 100 }";
+    String json12 = "{ \"action\": , \"time\":  }";
+    String json13 = "{ \"action\": null, \"time\": null }";
+    String json14 = "{ \"action\": \"jump\", \"time\": 0 }";
+    String json15 = "{ \"action\": \"jump\", \"time\": -8 }";
+    String json16 = "{ \"action\": \"jump\", \"time\":  }";
+    String json17 = "{ \"action\": \"jump\", \"time\": null }";
+
     /**
-     * Careful not to hurt ourselves here...
+     * Here we slay the dragons. Well, maybe spot them and then run for help.
      */
     @Test
-    public void shouldAnswerWithTrue()
+    public void addActionTest()
     {
-        assertTrue( true );
+        try {
+            jumpyLib.addAction(json1);
+            jumpyLib.addAction(json2);
+            jumpyLib.addAction(json3);
+            jumpyLib.addAction(json4);
+            jumpyLib.addAction(json5);
+            jumpyLib.addAction(json6);
+            jumpyLib.addAction(json7);
+            jumpyLib.addAction(json8);
+            jumpyLib.addAction(json9);
+            jumpyLib.addAction(json10);
+            assertTrue(true);
+        }
+        catch (Exception ex)
+        {
+            assertEquals("", ex.toString());
+        }
     }
 
-    //        Assert.assertTrue(jsonObj.isJsonObject());
-//        Assert.assertEquals("jump", jsonObj.get("action").getAsString());
-//        Assert.assertEquals(jsonObj.get("time").getAsInt(), 100);
+    @Test
+    public void getStatsTest() {
+        try {
+            jumpyLib.addAction(json1);
+            jumpyLib.addAction(json2);
+            jumpyLib.addAction(json3);
+            jumpyLib.addAction(json4);
+            jumpyLib.addAction(json5);
+            jumpyLib.addAction(json6);
+            jumpyLib.addAction(json7);
+            jumpyLib.addAction(json8);
+            jumpyLib.addAction(json9);
+            jumpyLib.addAction(json10);
+            String answer = jumpyLib.getStats();
+            assertEquals("[{\"action\":\"ski\",\"avg\":543},{\"action\":\"run\",\"avg\":309},{\"action\":\"swim\",\"avg\":732},{\"action\":\"jump\",\"avg\":180}]", answer);
+        }
+        catch (Exception ex) {
+            assertEquals("", ex.toString());
+        }
+    }
+
+    @Test
+    public void resetStats() {
+        try {
+            jumpyLib.addAction(json1);
+            jumpyLib.addAction(json2);
+            jumpyLib.addAction(json3);
+            jumpyLib.resetStats();
+            String answer = jumpyLib.getStats();
+            assertEquals("[]",answer);
+        }
+        catch (Exception ex)
+        {
+            assertEquals("", ex.toString());
+        }
+    }
+
+    @Test
+    public void emptyActionName() {
+        try {
+            jumpyLib.addAction(json11);
+        }
+        catch (Exception ex) {
+            assertEquals("java.lang.Exception: There is an error with the json data: { \"action\": \"\", \"time\": 100 }",ex.toString());
+        }
+    }
+
+    @Test
+    public void blankActionName() {
+        try {
+            jumpyLib.addAction(json12);
+        }
+        catch (Exception ex) {
+            assertEquals("java.lang.Exception: There is an error with the json data: { \"action\": , \"time\":  }",ex.toString());
+        }
+    }
+
+    @Test
+    public void nullActionNameTimeValue() {
+        try {
+            jumpyLib.addAction(json13);
+        }
+        catch (Exception ex) {
+            assertEquals("java.lang.Exception: There is an error with the json data: { \"action\": null, \"time\": null }",ex.toString());
+        }
+    }
+
+    @Test
+    public void zeroTimeValue() {
+        try {
+            jumpyLib.addAction(json14);
+        }
+        catch (Exception ex) {
+            assertEquals("java.lang.Exception: There is an error with the json data: { \"action\": \"jump\", \"time\": 0 }",ex.toString());
+        }
+    }
+
+    @Test
+    public void negativeTimeValue() {
+        try {
+            jumpyLib.addAction(json15);
+        }
+        catch (Exception ex) {
+            assertEquals("java.lang.Exception: There is an error with the json data: { \"action\": \"jump\", \"time\": -8 }",ex.toString());
+        }
+    }
+
+    @Test
+    public void emptyTimeValue() {
+        try {
+            jumpyLib.addAction(json16);
+        }
+        catch (Exception ex) {
+            assertEquals("java.lang.Exception: There is an error with the json data: { \"action\": \"jump\", \"time\":  }",ex.toString());
+        }
+    }
+
+    @Test
+    public void nullTimeValue() {
+        try {
+            jumpyLib.addAction(json17);
+        }
+        catch (Exception ex) {
+            assertEquals("java.lang.Exception: There is an error with the json data: { \"action\": \"jump\", \"time\": null }",ex.toString());
+        }
+    }
 
 }
