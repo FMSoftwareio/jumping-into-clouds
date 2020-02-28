a2f68f0ee2d187c3122ffd207039d0bedab022a1
Carl Fletcher <cfletch@gmail.com>
2020-02-27 19:34:30 -0500

    class for thread demo

---
+++ jumpy-fun-core/src/main/java/io/fmsoftware/jumpyfun/ThreadedKrisKross.java
@@ -0,0 +1,26 @@
+package io.fmsoftware.jumpyfun;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class ThreadedKrisKross extends Thread {
+    private static final Logger logger = LogManager.getLogger(ThreadedKrisKross.class);
+    private String jsonMsg;
+    private KrisKross jumpyLib;
+    private int threadNum;
+
+    ThreadedKrisKross(String jsonMsg, KrisKross jumpyLib, int threadNum) {
+        this.jsonMsg = jsonMsg;
+        this.jumpyLib = jumpyLib;
+        this.threadNum = threadNum;
+    }
+
+    public void run() {
+        try {
+            jumpyLib.addAction(jsonMsg);
+            System.out.println("ThreadNum: "+threadNum+" "+jumpyLib.getStats());
+        } catch (Exception ex) {
+            logger.fatal("{}", ex);
+        }
+    }
+}
