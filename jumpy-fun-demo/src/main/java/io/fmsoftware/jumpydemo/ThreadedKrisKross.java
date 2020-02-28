4d158c21fa04e95a2fc3a6d9672196fca858b31a
Carl Fletcher <cfletch@gmail.com>
2020-02-28 12:44:25 -0500

    created new module and package for the demo

--- jumpy-fun-demo/src/main/java/io/fmsoftware/jumpydemo/ThreadedKrisKross.java
+++ jumpy-fun-demo/src/main/java/io/fmsoftware/jumpydemo/ThreadedKrisKross.java
@@ -1,5 +1,6 @@
-package io.fmsoftware.jumpyfun;
+package io.fmsoftware.jumpydemo;
 
+import io.fmsoftware.jumpyfun.KrisKross;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
